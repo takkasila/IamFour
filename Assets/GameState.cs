35cc1b3d35c3cbdbe3ea026b78b48f727b51de8e
takkasila <takkasila@outlook.com>
2015-01-24 21:07:43 +0700

    GameState prototype

---
+++ Assets/GameState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TouchLog
+{
+    string name;
+    string tag;
+    public TouchLog(string name, string tag)
+    {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+    public string getTag()
+    {
+        return tag;
+    }
+}
+
+public class GameState : MonoBehaviour {
+
+    static List<TouchLog> touchLogList = new List<TouchLog>();
+
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public List<TouchLog> getTouchLogList()
+    {
+        return touchLogList;
+    }
+    public TouchLog getLastestTouchLog()
+    {
+        if (touchLogList.Count <= 0)
+            return null;
+        return touchLogList[touchLogList.Count - 1];
+    }
+
+
+}
